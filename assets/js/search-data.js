3a05340cf314e5f754f520baf942e4b175e4e148
TimSchopf <tim.schopf@t-online.de>
2025-04-08 11:59:07 +0000

    Deploying to gh-pages from @ TimSchopf/timschopf.github.io@1460dd744e205128a3c3a7a5bf48904a588cb47c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,10 +11,17 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "Publications",
-          description: "Publications in reversed chronological order.",
+          description: "Scientific publications in reversed chronological order.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
+          },
+        },{id: "nav-cv",
+          title: "CV",
+          description: "Short curriculum vitae.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
           },
         },{id: "post-a-post-with-plotly-js",
         
@@ -384,51 +391,31 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+            },},{id: "projects-createdata4ai",
+          title: 'CreateData4AI',
+          description: "Leveraging Domain Knowledge and Context Rules to Transform Large-Scale Unstructured Text Corpora into Structured and Annotated Datasets",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+              window.location.href = "/projects/cd4ai/";
+            },},{id: "projects-hackatum",
+          title: 'hackaTUM',
+          description: "The official annual hackathon of the Technical University of Munich",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+              window.location.href = "/projects/hackaTUM/";
+            },},{id: "projects-knowledge4retail",
+          title: 'Knowledge4Retail',
+          description: "Ecosystem for a Knowledge-Based Platform supporting In-Store Logistics",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+              window.location.href = "/projects/k4r/";
+            },},{id: "projects-nlp-kg",
+          title: 'NLP-KG',
+          description: "Natural Language Processing Knowledge Graph",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+              window.location.href = "/projects/nlp-kg/";
+            },},{id: "projects-tsaas",
+          title: 'TSaaS',
+          description: "Technology Scouting as a Service",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/tsaas/";
             },},{
         id: 'social-email',
         title: 'email',
