913e155c318be9087996c0f1c452816f005514b4
TimSchopf <7964959+TimSchopf@users.noreply.github.com>
2026-07-30 02:58:44 +0000

    Deploying to gh-pages from @ TimSchopf/timschopf.github.io@7c41a8c3a4662fff24b1e738fd416ae1f1920f3e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -47,32 +47,7 @@ ninja.data = [{
           section: "News",},{id: "news-multimodal-llms-are-much-better-at-verifying-claims-from-scientific-tables-than-equivalent-charts-our-new-preprint-investigates-why-️-️-do-models-fail-to-extract-chart-information-or-fail-to-use-it-effectively-once-extracted-check-it-out",
           title: 'Multimodal LLMs are much better at verifying claims from scientific tables than equivalent...',
           description: "",
-          section: "News",},{id: "projects-createdata4ai",
-          title: 'CreateData4AI',
-          description: "Leveraging Domain Knowledge and Context Rules to Transform Large-Scale Unstructured Text Corpora into Structured and Annotated Datasets",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/cd4ai/";
-            },},{id: "projects-hackatum",
-          title: 'hackaTUM',
-          description: "The official annual hackathon of the Technical University of Munich",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/hackaTUM/";
-            },},{id: "projects-knowledge4retail",
-          title: 'Knowledge4Retail',
-          description: "Ecosystem for a Knowledge-Based Platform supporting In-Store Logistics",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/k4r/";
-            },},{id: "projects-nlp-kg",
-          title: 'NLP-KG',
-          description: "Natural Language Processing Knowledge Graph",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/nlp-kg/";
-            },},{id: "projects-tsaas",
-          title: 'TSaaS',
-          description: "Technology Scouting as a Service",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/tsaas/";
-            },},{
+          section: "News",},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
